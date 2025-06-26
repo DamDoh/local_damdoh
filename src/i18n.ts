3ec4f2e03c3011ac190a01b383f86deb2f8bf9a1
Damdoh App <damdoh.org@gmail.com>
2025-06-26 11:22:32 +0000

    Error: Couldn't find next-intl config file. Please follow the instructio

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,1 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
- 
-// Can be imported from a shared config
-const locales = ['en', 'es', 'zh', 'fr', 'ar', 'pt', 'hi', 'ru', 'id', 'de', 'tr', 'ja', 'km', 'th', 'ko', 'vi', 'ms'];
- 
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
- 
-  return {
-    // Using a relative path is more robust for build tools.
-    messages: (await import(`./messages/${locale}.json`)).default
-  };
-});
+// This file has been moved to the project root to resolve a build issue.
