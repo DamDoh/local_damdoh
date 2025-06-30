627135767632b746e01dba3516f908ab0ebfacd1
Sok Chuob <chuob.sok@gmail.com>
2025-06-30 12:23:13 +0000

    Yes

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,4 +1,3 @@
-
 /**
  * @fileoverview This is the main entry point for all Firebase Cloud Functions.
  * It initializes the Firebase Admin SDK and exports all the functions from other
@@ -7,29 +6,19 @@
 
 import * as admin from "firebase-admin";
 
-// Initialize the Firebase Admin SDK.
-// This must be done only once, at the entry point of your functions.
 admin.initializeApp();
 
-// Now, re-export all the functions from your other modules.
-// The Firebase CLI will discover and deploy these exported functions.
-
-export * from './dashboard_data';
-export * from './module1';
-export * from './module3';
-export * from './module4';
-export * from './module5';
-export * from './module6';
-export * from './module7';
-export * from './module8';
-export * from './module9';
-export * from './module10';
-export * from './module11';
-export * from './module12';
-export * from './notifications';
-export * from './offline_sync';
-export * from './search';
-export * from './profiles';
-export * from './types';
-
-    
+export * from "./traceability";
+export * from "./profiles";
+export * from "./farm-management";
+export * from "./marketplace";
+export * from "./community";
+export * from "./ai-and-analytics";
+export * from "./financial-services";
+export * from "./knowledge-hub";
+export * from "./api-gateway";
+export * from "./regulatory-and-compliance";
+export * from "./insurance";
+export * from "./sustainability";
+export * from "./notifications";
+export * from "./types";
