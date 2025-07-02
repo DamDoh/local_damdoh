867696bb506c4c8adc590f5928d28232e890636f
Sok Chuob <chuob.sok@gmail.com>
2025-07-02 06:27:58 +0000

    continue

--- damdoh-app/docs/ARCHITECTURE.md
+++ damdoh-app/docs/ARCHITECTURE.md
@@ -1,3 +1,2 @@
-// This file is obsolete and has been removed to avoid code duplication.
-// The active architecture document is in /docs/
+// This file is obsolete. The active architecture documents are in the root /docs/ directory.
 // This file can be safely deleted.
--- damdoh-app/firebase/functions/.eslintrc.js
+++ damdoh-app/firebase/functions/.eslintrc.js
@@ -1,3 +1,2 @@
-// This file is obsolete and has been removed to avoid code duplication.
-// The active .eslintrc.js for Firebase Functions is located at /firebase/functions/.eslintrc.js
+// This file is obsolete. The active .eslintrc.js for Firebase Functions is located at /firebase/functions/.eslintrc.js
 // This file can be safely deleted.
