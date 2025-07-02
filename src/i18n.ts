477734e513f249790911d559e3587b808aa21d0e
Sok Chuob <chuob.sok@gmail.com>
2025-07-02 23:49:15 +0000

    Error: Couldn't find next-intl config file. Please follow the instructio

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,12 @@
+import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
+import {locales} from './lib/i18n-constants';
  
-// Using a dynamic import for messages is recommended
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`./messages/${locale}.json`)).default
-}));
+export default getRequestConfig(async ({locale}) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) notFound();
+ 
+  return {
+    messages: (await import(`../messages/${locale}.json`)).default
+  };
+});
