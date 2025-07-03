4153da9e1a18020d10bbd93d78b6c9f4a46d808f
Sok Chuob <chuob.sok@gmail.com>
2025-07-03 08:54:26 +0000

    Consider that Since the dawn of programing you know every piece of code

--- i18n.ts
+++ i18n.ts
@@ -1,7 +1,8 @@
 
 import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
-
+ 
+// Can be imported from a shared config
 const locales = ['ar', 'de', 'en', 'es', 'fr', 'hi', 'id', 'ja', 'km', 'ko', 'ms', 'pt', 'ru', 'th', 'tr', 'vi', 'zh'];
  
 export default getRequestConfig(async ({locale}) => {
---
+++ src/i18n.ts
@@ -0,0 +1,2 @@
+// This file is obsolete. The correct i18n configuration now resides in the project root.
+// This file will be deleted.
