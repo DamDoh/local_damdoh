9feafb2266c44f11a405f4fcf4b893375fc6534b
Sok Chuob <chuob.sok@gmail.com>
2025-07-04 07:36:39 +0000

    continue

--- src/lib/i18n-constants.ts
+++ src/lib/i18n-constants.ts
@@ -1,2 +1,32 @@
-// This file is obsolete and has been removed to avoid configuration conflicts.
-// The primary i18n constants are now in `src/i18n-config.ts`.
+
+import { STAKEHOLDER_ROLES, LISTING_TYPES, AGRI_EVENT_TYPES, FINANCIAL_SERVICE_TYPES, INSURANCE_SERVICE_TYPES } from './constants';
+
+export const getStakeholderRoles = (t: any) =>
+  STAKEHOLDER_ROLES.map(role => ({
+    value: role,
+    label: t(role.replace(/[\s/()]+/g, '')) // Create a key like 'FieldAgentAgronomistDamDohInternal'
+  }));
+  
+export const getListingTypes = (t: any) =>
+  LISTING_TYPES.map(type => ({
+    value: type,
+    label: t(type.toLowerCase())
+  }));
+  
+export const getAgriEventTypes = (t: any) =>
+  AGRI_EVENT_TYPES.map(type => ({
+    value: type,
+    label: t(type.replace(/\s/g, ''))
+  }));
+
+export const getFinancialServiceTypes = (t: any) =>
+  FINANCIAL_SERVICE_TYPES.map(type => ({
+    value: type,
+    label: t(type.replace(/_/g, ''))
+  }));
+
+export const getInsuranceServiceTypes = (t: any) =>
+  INSURANCE_SERVICE_TYPES.map(type => ({
+    value: type,
+    label: t(type.replace(/_/g, ''))
+  }));
