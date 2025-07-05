557cdb3451b34c4e224a0977ee78f04f310df632
Sok Chuob <chuob.sok@gmail.com>
2025-07-05 07:39:46 +0000

    Someone give me this instructions:An "i18n.ts" file causing an error typ

---
+++ src/i18n.d.ts
@@ -0,0 +1,9 @@
+// Use type safe message keys with `next-intl`
+// This file provides TypeScript with a clear structure for your translation files,
+// preventing type-checking errors that can crash the application.
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+type Messages = typeof import('./messages/en.json');
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+declare interface IntlMessages extends Messages {}
