e59c7b44e57e8e80f20a277344874da5f95730e4
Sok Chuob <chuob.sok@gmail.com>
2025-07-08 04:07:43 +0000

    Great, Yes, continue the audit, repair, and enhancement process on this

--- src/components/marketplace/ItemCard.tsx
+++ src/components/marketplace/ItemCard.tsx
@@ -1,4 +1,6 @@
 
+"use client";
+
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from 'next/image';
 import { Button } from "@/components/ui/button";
