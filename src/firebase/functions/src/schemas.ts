982ac421e238c497da0084e9e30b0cf1e647a985
Sok Chuob <chuob.sok@gmail.com>
2025-07-10 14:05:32 +0000

    Continue to the next logical step

--- src/firebase/functions/src/schemas.ts
+++
@@ -1,5 +0,0 @@
-
-// This file is deprecated. The single source of truth for schemas is now located in `/src/lib/schemas.ts`.
-// It is recommended to import schemas from '@/lib/schemas' instead.
-// This file is kept to avoid breaking build processes but should not be used for new development.
-export {};
