904c556d5f55299177d1a9885a4244fc5faa17c6
Sok Chuob <chuob.sok@gmail.com>
2025-07-10 23:17:13 +0000

    Hi, don't you realize that you fix the translation of a phage but you de

--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,11 +1,12 @@
 
-export const locales = ['en', 'fr', 'de', 'km'] as const;
+export const locales = ['en', 'fr', 'de', 'km', 'th'] as const;
 
 export const localeNames: Record<typeof locales[number], string> = {
   en: "English",
   fr: "Français",
   de: "Deutsch",
   km: "ភាសាខ្មែរ",
+  th: "ไทย",
 };
 
     
