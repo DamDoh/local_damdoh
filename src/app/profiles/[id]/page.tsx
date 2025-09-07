653af11e03ff7b293914119824609cf591ac72d5
Sok Chuob <chuob.sok@gmail.com>
2025-09-07 08:02:19 +0000

    Great, please continue to the next logical step.

--- src/app/profiles/[id]/page.tsx
+++ src/app/profiles/[id]/page.tsx
@@ -370,3 +370,13 @@ export default function ProfileDetailPage() {
     </div>
   );
 }
+
+```
+    <file>src/firebase/functions/src/profiles.ts</file>
+    <content><![CDATA[
+
+// This file is deprecated. All profile-related callable functions have been moved to their own dedicated files
+// (e.g., activity.ts, network.ts) and user lifecycle triggers (onCreate, onDelete) are now in utils.ts.
+// This file is kept temporarily to avoid breaking any lingering imports but should
+// not be used for new development. It will be removed in a future cleanup.
+export {};
