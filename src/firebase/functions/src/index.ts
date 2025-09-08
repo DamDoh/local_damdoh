5412a8335a2e19455eb997f592d6302a40cbe5db
Sok Chuob <chuob.sok@gmail.com>
2025-09-08 05:48:49 +0000

    Great, Please continue to the next logical step of rebuilding the next s

--- src/firebase/functions/src/index.ts
+++ src/firebase/functions/src/index.ts
@@ -1,5 +1,4 @@
 
-
 /**
  * @fileoverview This is the main entry point for all Firebase Cloud Functions.
  * It initializes the Firebase Admin SDK and exports all the functions from other
@@ -22,7 +21,6 @@ if (admin.apps.length === 0) {
 import * as activityFunctions from "./activity";
 import * as agriEventsFunctions from "./agri-events";
 import * as agroTourismFunctions from "./agro-tourism";
-import * as aiAndAnalyticsFunctions from "./ai-and-analytics";
 import * as aiServicesFunctions from "./ai-services";
 import * as apiKeyFunctions from "./api-keys";
 import * as apiGatewayFunctions from "./api-gateway";
@@ -43,11 +41,11 @@ import * as messageFunctions from "./messages";
 import * as networkFunctions from "./network";
 import * as notificationFunctions from "./notifications";
 import * as offlineSyncFunctions from "./offline_sync";
+import * as userFunctions from "./user";
+import * as utilsFunctions from "./utils";
 import * as regulatoryFunctions from "./regulatory-and-compliance";
 import * as searchFunctions from "./search";
 import * as sustainabilityFunctions from "./sustainability";
-import * as userFunctions from "./user";
-import * as utilsFunctions from "./utils";
 import * as geospatialFunctions from "./geospatial";
 import * as universalIdFunctions from "./universal-id";
 
@@ -55,7 +53,6 @@ import * as universalIdFunctions from "./universal-id";
 export const activity = activityFunctions;
 export const agriEvents = agriEventsFunctions;
 export const agroTourism = agroTourismFunctions;
-export const aiAndAnalytics = aiAndAnalyticsFunctions;
 export const aiServices = aiServicesFunctions;
 export const apiKeys = apiKeyFunctions;
 export const apiGateway = apiGatewayFunctions;
@@ -87,4 +84,3 @@ export const universalId = universalIdFunctions;
 
 // Export the Express app as a Cloud Function for Cloud Run services
 export const api = functions.https.onRequest(expressApp);
-
