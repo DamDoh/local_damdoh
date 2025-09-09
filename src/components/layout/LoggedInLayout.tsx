25eac21c6f139965f18f77f22be56718c90420d6
Sok Chuob <chuob.sok@gmail.com>
2025-09-09 14:48:34 +0000

    We are getting there, please continue.

--- src/components/layout/LoggedInLayout.tsx
+++ src/components/layout/LoggedInLayout.tsx
@@ -11,7 +11,9 @@ import { useIsMobile } from '@/hooks/use-mobile';
 import { MobileBottomNavigation } from './MobileBottomNavigation';
 import { LandingPageLayout } from './LandingPageLayout';
 import { DashboardLeftSidebar } from '../dashboard/DashboardLeftSidebar';
-import { DashboardRightSidebar } from '../dashboard/hubs/DashboardRightSidebar';
+import { DashboardRightSidebar } from '../dashboard/DashboardRightSidebar';
+import { SidebarProvider } from '../ui/sidebar';
+import { AppSidebarNav } from './AppSidebarNav';
 
 export function LoggedInLayout({ children }: { children: React.ReactNode }) {
     const { user, loading } = useAuth();
@@ -31,24 +33,26 @@ export function LoggedInLayout({ children }: { children: React.ReactNode }) {
     }
 
     return (
-        <div className="flex flex-col min-h-screen bg-background">
-            <AppHeader />
-            <div className="flex-grow container mx-auto max-w-screen-2xl px-4 py-6 md:px-6 lg:px-8 print:p-0">
-                <div className="grid md:grid-cols-12 gap-6 items-start">
-                    <aside className="hidden md:block md:col-span-3 lg:col-span-2 sticky top-20">
-                        <DashboardLeftSidebar />
-                    </aside>
-                    <main className="md:col-span-9 lg:col-span-7 space-y-6">
-                        {children}
-                    </main>
-                    <aside className="hidden lg:block lg:col-span-3 sticky top-20">
-                        <DashboardRightSidebar />
-                    </aside>
+        <SidebarProvider>
+            <div className="flex flex-col min-h-screen bg-background">
+                <AppHeader />
+                <div className="flex-grow container mx-auto max-w-screen-2xl px-4 py-6 md:px-6 lg:px-8 print:p-0">
+                    <div className="grid md:grid-cols-12 gap-6 items-start">
+                        <aside className="hidden md:block md:col-span-3 lg:col-span-2 sticky top-20">
+                           <DashboardLeftSidebar />
+                        </aside>
+                        <main className="md:col-span-9 lg:col-span-7 space-y-6">
+                            {children}
+                        </main>
+                        <aside className="hidden lg:block lg:col-span-3 sticky top-20">
+                            <DashboardRightSidebar />
+                        </aside>
+                    </div>
                 </div>
+                <OfflineIndicator />
+                <Toaster />
+                {isMobile && <MobileBottomNavigation />}
             </div>
-            <OfflineIndicator />
-            <Toaster />
-            {isMobile && <MobileBottomNavigation />}
-        </div>
+        </SidebarProvider>
     );
 }
