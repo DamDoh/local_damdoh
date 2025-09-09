cc14ab9b581af7e490246c72c125f1889d8982ee
Sok Chuob <chuob.sok@gmail.com>
2025-09-09 11:05:48 +0000

    Encountering this problem at the landing page, but we need side bar in t

--- src/components/layout/LandingPageLayout.tsx
+++ src/components/layout/LandingPageLayout.tsx
@@ -4,16 +4,25 @@
 import { AppHeader } from "@/components/layout/AppHeader";
 import { AppFooter } from "@/components/layout/AppFooter";
 import { Toaster } from "@/components/ui/toaster";
+import { SidebarProvider, SidebarInset, Sidebar } from "@/components/ui/sidebar";
+import { AppSidebarNav } from "./AppSidebarNav";
 
 export function LandingPageLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col min-h-screen">
-            <AppHeader />
-            <main className="flex-grow">
-                {children}
-            </main>
-            <AppFooter />
-            <Toaster />
-        </div>
+      <SidebarProvider>
+        <Sidebar>
+            <AppSidebarNav />
+        </Sidebar>
+        <SidebarInset>
+            <div className="flex flex-col min-h-screen">
+                <AppHeader />
+                <main className="flex-grow">
+                    {children}
+                </main>
+                <AppFooter />
+                <Toaster />
+            </div>
+        </SidebarInset>
+      </SidebarProvider>
     );
 }
